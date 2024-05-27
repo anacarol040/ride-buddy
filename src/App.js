9ae2df1292e8b3bee289bd7dcfc15a73971ac1cd
Ana de Oliveira <anacarol040@gmail.com>
2024-05-27 15:08:21 -0600

    Include date and time selectors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import {React, useState} from "react";
-import {Form, Select} from "antd";
+import {React, useState} from 'react';
+import {Button, Form, Select, Radio, DatePicker, TimePicker} from 'antd';
 
 const { Option } = Select;
 const { Item } = Form
@@ -8,8 +8,12 @@ const { Item } = Form
 const LandingPage: React.FC = () => {
 
     const cities = ['Airdrie', 'Banff', 'Calgary', 'Canmore', 'Vancouver'];
+
     const [from, setFrom] = useState('');
     const [destination, setDestination] = useState('');
+    const [formLayout, setFormLayout] = useState('passenger');
+    const [selectedDate, setSelectedDate] = useState(null);
+    const [selectedTime, setSelectedTime] = useState(null);
 
     const handleFromChange = (value) => {
         setFrom(value);
@@ -19,9 +23,38 @@ const LandingPage: React.FC = () => {
         setDestination(value);
     }
 
+    const handleFormLayoutChange = (layout) => {
+        setFormLayout(layout.target.value);
+        resetFields();
+    };
+
+    const handleSelectedDateChange = (date) => {
+        setSelectedDate(date);
+    }
+
+    const handleSelectedTimeChange = (time) => {
+        setSelectedTime(time);
+    }
+
+    const resetFields = () => {
+        console.log(from, destination)
+        setSelectedDate(null)
+        setSelectedTime(null)
+    }
+
     return (
-        <Form>
-            <Item name='to' label='From: '>
+        <Form
+            labelCol={{span: 4}}
+            wrapperCol={{span: 14}}
+            size='large'
+        >
+            <Item label='I want to' name='layout'>
+                <Radio.Group value={formLayout} onChange={handleFormLayoutChange}>
+                    <Radio.Button value='passenger'> Find a Ride </Radio.Button>
+                    <Radio.Button value='driver'> Post a Trip </Radio.Button>
+                </Radio.Group>
+            </Item>
+            <Item label='From:' name='from' >
                 <Select
                     placeholder='Select starting location'
                     value={from}
@@ -34,7 +67,7 @@ const LandingPage: React.FC = () => {
                         ))}
                 </Select>
             </Item>
-            <Item name='to' label='To: '>
+            <Item label='To: ' name='to'>
                 <Select
                     placeholder='Select destination'
                     value={destination}
@@ -46,6 +79,19 @@ const LandingPage: React.FC = () => {
                         </Option>
                     ))}
                 </Select>
+            </Item>
+            <Item label='Select a date:'>
+                <DatePicker value={selectedDate} onChange={handleSelectedDateChange} />
+            </Item>
+            {formLayout === 'driver' && (
+                <Item label='Estimated time'>
+                    <TimePicker format='HH:mm' value={selectedTime} onChange={handleSelectedTimeChange}/>
+                </Item>
+            )}
+            <Item label=''  wrapperCol={{ offset: 4, span: 16 }}>
+                <Button type='primary' htmlType='submit'>
+                    Submit
+                </Button>
             </Item>
         </Form>
   )
